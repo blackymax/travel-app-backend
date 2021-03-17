6d7890aed100657141818065430ba2aced38b55f
blackymax <maxmin3007@gmail.com>
2021-03-17 20:17:27 +0300

    fix: paths

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const countrySchema = require('./models/countrySchema');
 
 const app = express();
 
-const PORT = config.get('port') || 5000;
+const PORT = 5000;
 
 app.use(express.json({ limit: 15728640 }));
 
